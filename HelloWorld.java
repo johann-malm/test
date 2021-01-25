70a111276923b655c302a1dd91ae21c91d8e304e
Johann Malm <hej@johannmalm.com>
2021-01-25 11:44:59 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello World");
     System.out.println("Yazan Ibesh");
+    System.out.println("Yazan Ibesh2");
   }
 }
